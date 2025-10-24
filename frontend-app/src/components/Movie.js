1d6bef6aa23cf512c0dbd71b426a91163d841676
yuriferreiracodes <yuriferreiradeveloper@gmail.com>
2025-10-24 20:18:50 -0300

    feat: configure values inside movie component

--- frontend-app/src/components/Movie.js
+++ frontend-app/src/components/Movie.js
@@ -1,10 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 
 const Movie = () => {
+    const [movie, setMovie] = useState({});
+
+    let { id } = useParams();
+
+    useEffect(() =>  {
+        let myMovie = {
+            id: 1,
+            title: "Highlander",
+            release_date: "1986-03-07",
+            runtime: 116,
+            mpaa_rating: "R",
+            description: "A Scottish warrior learns he cannot die after a battle wound. Many years later in New York, he must fight others like him until only one remains.",
+        }
+        setMovie(myMovie);
+    }, [id])
 
     return(
         <div>
-            <h2>Movie</h2>
+            <h2>Movie: {movie.title}</h2>
+            <small><em>{movie.release_date}, {movie.runtime} minutes, Rated: {movie.mpaa_rating}</em></small>
             <hr />
+            <p>{movie.description}</p>
         </div>
     )
 }
