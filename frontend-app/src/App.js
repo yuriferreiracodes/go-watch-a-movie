c365d03a8bdb7a9953f4321b45df7dc3dc66802e
yuriferreiracodes <yuriferreiradeveloper@gmail.com>
2025-10-25 01:59:02 -0300

    feat: create first version of login logic

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { Link, Outlet } from 'react-router-dom';
 
 function App() {
+
+  const [jwtToken, setJwtToken] = useState("");
+
+
   return (
     <div className="container">
       <div className="row">
@@ -8,7 +13,10 @@ function App() {
           <h1 className="mt-3">Go Watch a Movie!</h1>
         </div>
         <div className="col text-end">
-          <Link to="#!"><span className="badge bg-success">Login</span></Link>
+          {jwtToken === ""
+            ? <Link to="#!"><span className="badge bg-success">Login</span></Link>
+            : <Link to="#!"><span className="badge bg-danger">Logout</span></Link>
+          }
         </div>
         <hr className="mb-3"></hr>
       </div>
@@ -20,9 +28,13 @@ function App() {
               <Link to="/" className="list-group-item list-group-item-action">Home</Link>
               <Link to="/movies" className="list-group-item list-group-item-action">Movies</Link>
               <Link to="/genres" className="list-group-item list-group-item-action">Genres</Link>
-              <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
-              <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalogue</Link>
-              <Link to="/graphql" className="list-group-item list-group-item-action">GraphQL</Link>
+              {jwtToken !== "" && 
+                <>
+                <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
+                <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalogue</Link>
+                <Link to="/graphql" className="list-group-item list-group-item-action">GraphQL</Link>
+                </>
+              }
             </div>
           </nav>
         </div>
