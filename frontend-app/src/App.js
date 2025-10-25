dd81712dd6544ddf8d65e4aeff6823e622eea397
yuriferreiracodes <yuriferreiradeveloper@gmail.com>
2025-10-25 02:49:26 -0300

    feat: configure jwt state

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -39,7 +39,9 @@ function App() {
           </nav>
         </div>
         <div className="col-md-10">
-            <Outlet />
+            <Outlet context={{
+              jwtToken, setJwtToken,
+            }}/>
         </div>
       </div>
     </div>
