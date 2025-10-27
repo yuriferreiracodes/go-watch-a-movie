fc795649ac806adf754f819b3f8ae3597729742b
yuriferreiracodes <yuriferreiradeveloper@gmail.com>
2025-10-27 06:36:44 -0300

    feat: create new go http server

---
+++ backend-app/cmd/api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string 
+}
+
+func main() {
+	var app application 
+
+	app.Domain = "example.com"
+
+	log.Printf("Starting application on port %d", port)
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
