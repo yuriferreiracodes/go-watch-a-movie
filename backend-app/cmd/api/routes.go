95674c5e07e94ba315eddc2227036ad1d9c46f6d
yuriferreiracodes <yuriferreiradeveloper@gmail.com>
2025-10-29 18:12:51 -0300

    refactor: recreate middleware such as new layer

--- backend-app/cmd/api/routes.go
+++ backend-app/cmd/api/routes.go
@@ -10,25 +10,10 @@ func (app *application) routes() http.Handler {
     mux := chi.NewRouter()
     
     mux.Use(middleware.Recoverer)
-    mux.Use(enableCORS) // Adicione este middleware
+    mux.Use(EnableCORS)
     
     mux.Get("/", app.Home)
     mux.Get("/movies", app.AllMovies)
     
     return mux
-}
-
-func enableCORS(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-        
-        if r.Method == "OPTIONS" {
-            w.WriteHeader(http.StatusOK)
-            return
-        }
-        
-        next.ServeHTTP(w, r)
-    })
 }
