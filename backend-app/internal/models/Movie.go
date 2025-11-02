42b1bfe381fff39f52a1e0948a1dedf04b1236aa
yuriferreiracodes <yuriferreiradeveloper@gmail.com>
2025-11-02 15:17:22 -0300

    refactor: update movie model

--- backend-app/internal/models/Movie.go
+++ backend-app/internal/models/Movie.go
@@ -3,13 +3,14 @@ package models
 import "time"
 
 type Movie struct {
-	ID          int       `json:"id"`
-	Title       string    `json:"title"`
-	ReleaseDate time.Time `json:"release_date"`
-	RunTime     int       `json:"runtime"`
-	MPAARating  string    `json:"mpaa_rating"`
-	Description string    `json:"description"`
-	Image       string    `json:"image"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	ReleaseYear int        `json:"release_year"`
+	Genre       string     `json:"genre"`
+	Director    string     `json:"director"`
+	Rating      float64    `json:"rating"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at"`
 }
