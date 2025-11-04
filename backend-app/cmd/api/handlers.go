9505c377210963c5135210b78384a5ad8a3ceebe
yuriferreiracodes <yuriferreiradeveloper@gmail.com>
2025-11-04 08:39:22 -0300

    feat: add handler to take data from database

--- backend-app/cmd/api/handlers.go
+++ backend-app/cmd/api/handlers.go
@@ -29,53 +29,18 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
-    var movies []models.Movie
-    
-	highlander := models.Movie{
-		ID:          1,
-		Title:       "Highlander",
-		ReleaseDate: time.Date(1986, time.March, 7, 0, 0, 0, 0, time.UTC),
-		RunTime:     116,
-		MPAARating:  "R",
-		Description: "An immortal Scottish swordsman must confront his centuries-old nemesis in a battle for the Prize.",
-		Image:       "highlander.jpg",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
+	db := db.Connect()
+	defer db.Close()
+
+	rows, _ := db.Query("SELECT id, title, description, release_date, runtime, mpaa_rating, image FROM movies")
+	defer rows.Close()
+
+	var movies []models.Movie
+	for rows.Next() {
+		var m models.Movie
+		rows.Scan(&m.ID, &m.Title, &m.Description, &m.ReleaseDate, &m.RunTime, &m.MPAARating, &m.Image)
+		movies = append(movies, m)
 	}
 
-	matrix := models.Movie{
-		ID:          2,
-		Title:       "The Matrix",
-		ReleaseDate: time.Date(1999, time.March, 31, 0, 0, 0, 0, time.UTC),
-		RunTime:     136,
-		MPAARating:  "R",
-		Description: "A computer hacker learns the true nature of reality and his role in the war against its controllers.",
-		Image:       "matrix.jpg",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-
-	underworld := models.Movie{
-		ID:          3,
-		Title:       "Underworld",
-		ReleaseDate: time.Date(2003, time.September, 19, 0, 0, 0, 0, time.UTC),
-		RunTime:     121,
-		MPAARating:  "R",
-		Description: "A vampire warrior becomes embroiled in a war between vampires and werewolves.",
-		Image:       "underworld.jpg",
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-    
-	movies = append(movies, highlander, matrix, underworld)  
-
-    out, err := json.Marshal(movies)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write(out)
+	json.NewEncoder(w).Encode(movies)
 }
